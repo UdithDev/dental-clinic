513ddf65633f3465d17266cdf08442b9999fe105
Udith_Dev_Wee <daveudith@gmail.com>
2023-12-29 19:32:17 +0530

    update landing page in front end

--- frontend/dental-frontend/src/components/LandingPage.tsx
+++ frontend/dental-frontend/src/components/LandingPage.tsx
@@ -5,11 +5,19 @@ function HeroSection(){
                 <h1 className="font-medium text-black/80 text-[20px] leading-3">
                     Welcome to
                 </h1>
-                <p>
+                <p className="font-semibold text-black/90 text-[60px]">
                     Denta Clinic
-                    <span className=" text">.</span>
+                    <span className=" text-accent animate-pulse">.</span>
                 </p>
-
+                <p className="text-sm text-black/80 lg:font-thin text-center">
+                    Your premier destination for dental care. We understand that visiting
+                    the dentist can be a stressful experience, which is why our team of
+                    highly skilled professionals is dedicated to providing you with 
+                    personalized care in a comfortable and welcoming environment.
+                </p>
+            </div>
+            <div className="">
+                <img src="" alt="" />
             </div>
 
         </div>
