c69a32971af6579e8cac62a38c9040b726c9e6f6
Udith_Dev_Wee <daveudith@gmail.com>
2024-01-06 14:25:45 +0530

    content updated

---
+++ frontend/dental-frontend/src/components/inventory/ItemForm.tsx
@@ -0,0 +1,11 @@
+function ItemForm(){
+    return(
+        <div>
+            <div className="">
+                <label htmlFor="item-name">Item Name</label>
+                <input type="text" />
+            </div>
+        </div>
+    )
+}
+export default ItemForm;
--- frontend/dental-frontend/src/pages/inventory.tsx
+++ frontend/dental-frontend/src/pages/inventory.tsx
@@ -1,15 +1,33 @@
+import { useState, useRef } from "react";
+import AddItem from "../components/inventory/AddItem";
 import Welcome from "../components/inventory/Welcome";
+import ItemForm from "../components/inventory/ItemForm";
 
-function Inventory(){
-    return(
-        <div className="flex flex-col">
-            <div className="inline-block min-w-full">
-                <Welcome/>
-            </div>
+function Inventory() {
+  const [toggle, setToggle] = useState(false);
+  const printRef = useRef();
 
+  return (
+    <div className="flex flex-col">
+      <div className="inline-block min-w-full">
+        <Welcome />
+        <div className="mb-10" />
+
+        <div className="mb-10" />
+        <AddItem />
+        <button
+          onClick={() => setToggle(!toggle)}
+          type="button"
+          className="text-sm font-medium bg-accent px-2 w-24 h-8 rounded-full text-white my-4"
+        >
+          Add Item
+        </button>
+        <div ref={printRef} className={`${toggle ? "" : "hidden"}`}>
+          <ItemForm />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-
 
 export default Inventory;
