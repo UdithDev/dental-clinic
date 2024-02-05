9e68e191286837bcc663c59548eeefc4ebe02da7
Udith_Dev_Wee <daveudith@gmail.com>
2024-02-05 16:21:18 +0530

    add vebdor details handlechange i updateItem page

--- frontend/dental-frontend/src/components/inventory/UpdateItem.tsx
+++ frontend/dental-frontend/src/components/inventory/UpdateItem.tsx
@@ -146,6 +146,26 @@ function UpdateItem() {
           </p>
         </div>
       </div>
+      <div className="felx flex-wrap -mx-3 md:mb-6">
+        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="vendor-details"
+          >
+            Vendor Details
+          </label>
+          <input
+            onChange={handleChange("vendor_details")}
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="vendor-details"
+            type="text"
+            placeholder="Asprin Inc."
+            value={state.vendor_details}
+          />
+        </div>
+      </div>
+
+      
     </form>
   );
 }
