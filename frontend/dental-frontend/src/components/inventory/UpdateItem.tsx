fa5005a43f90a0ec37d01ebe3210d7232744d0e2
Udith_Dev_Wee <daveudith@gmail.com>
2024-02-05 11:33:41 +0530

    create updateItem page

---
+++ frontend/dental-frontend/src/components/inventory/UpdateItem.tsx
@@ -0,0 +1,45 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+function UpdateItem() {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  useEffect(() => {
+    if (success) {
+      setError("");
+    }
+  }, [success]);
+
+  const [state, setState] = useState({
+    item_name: "",
+    sku: "",
+    serial_number: "",
+    vendor_details: "",
+    item_location: "",
+    expiry_date: "",
+    quantity_available: "",
+    minimum_stock: "",
+  });
+
+  const getItembyId = async () => {
+    const response = await axios.get(
+        `${import.meta.env.VITE_API}/inventory/${id}`
+    );
+
+    setState({
+        item_name: response?.data?.item_name,
+        sku: response?.data?.sku,
+        serial_number: response?.data?.serial_number,
+        vendor_details: response?.data?.vendor_details,
+        item_location: response?.data?.item_location,
+        expiry_date: response?.data?.expiry_date,
+        quantity_available: response?.data?.quantity_available,
+        minimum_stock: response?.data?.minimum_stock,
+    })
+  };
+}
+export default UpdateItem;
