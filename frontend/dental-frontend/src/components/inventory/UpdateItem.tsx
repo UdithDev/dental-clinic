e7374d2ad2704dd919689de30146051bbab6d9d0
Udith_Dev_Wee <daveudith@gmail.com>
2024-02-05 15:47:55 +0530

    updated Update Item Page

--- frontend/dental-frontend/src/components/inventory/UpdateItem.tsx
+++ frontend/dental-frontend/src/components/inventory/UpdateItem.tsx
@@ -85,7 +85,68 @@ function UpdateItem() {
     }
   };
 
-
-  
+  return (
+    <form
+      onChange={() => setSuccess("")}
+      onSubmit={handleSubmt}
+      className="w-full lg:max-w-2xl bg-pearlWhite/30 p-5 rounded-lg"
+    >
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="item-name"
+          >
+            Item Name
+          </label>
+          <input
+            onChange={handleChange("item_name")}
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            id="item-name"
+            type="text"
+            placeholder="Face Masks"
+            value={state.item_name}
+          />
+        </div>
+        <div className="w-full md:w-1/2 px-3">
+          <label
+            className="block uppercase  tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="sku"
+          >
+            SKU
+          </label>
+          <input
+            onChange={handleChange("sku")}
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="sku"
+            type="text"
+            placeholder="XYZ12345"
+            value={state.sku}
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="serial-no"
+          >
+            Serial No
+          </label>
+          <input
+            onChange={handleChange("serial_number")}
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="serial-no"
+            type="number"
+            placeholder="394857739"
+            value={state.serial_number}
+          />
+          <p className="text-gray-600 text-xs italic">
+            Serial Number must be a unique set of numbers for each item
+          </p>
+        </div>
+      </div>
+    </form>
+  );
 }
 export default UpdateItem;
