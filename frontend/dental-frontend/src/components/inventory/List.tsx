3a78f0b52980c181a6be8571176223214d08d7ff
Udith_Dev_Wee <daveudith@gmail.com>
2024-02-28 00:15:01 +0530

    create exportPDF function in list component

--- frontend/dental-frontend/src/components/inventory/List.tsx
+++ frontend/dental-frontend/src/components/inventory/List.tsx
@@ -1,3 +1,33 @@
-function List(){
-    
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
+
+function List() {
+  const [toggle, setToggle] = useState(true);
+  const [inventory, setInventory] = useState([]);
+  const [query, setQuery] = useState("");
+  const table = useRef(null);
+
+  const getInventory = async () => {
+    const { data } = await axios.get(`${import.meta.env.VITE_API}/inventory`);
+
+    setInventory(data);
+  };
+
+  useEffect(() => {
+    getInventory();
+  });
+
+  const exportPDF = () => {
+    let element = table.current;
+    if (element !== null) {
+      savePDF(element, {
+        paperSize: "a4",
+        margin: "2cm",
+        landscape: true,
+        fileName: "CurrentInventory",
+        scale: 0.5,
+      });
+    }
+  };
 }
+export default List;
