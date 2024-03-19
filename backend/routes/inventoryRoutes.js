256d53ca77007eddf50065fef538f224bf318a23
Udith_Dev_Wee <daveudith@gmail.com>
2024-03-19 16:57:25 +0530

    update router and api

--- backend/routes/inventoryRoutes.js
+++ backend/routes/inventoryRoutes.js
@@ -1,2 +1,31 @@
 const express = require("express");
 
+//validators
+const { inventoryCreateValidator } = require("../validators/inventory");
+const { runValidation } = require("../validators");
+
+//controllers
+
+const {
+  getInventory,
+  createInventoryItem,
+  deleteInventoryItem,
+  sendEmail,
+  updateInventoryItem,
+  getItemById,
+} = require("../controllers/inventoryController");
+
+const router = express.Router();
+
+router.route("/").get(getInventory);
+router
+  .route("/add")
+  .post(inventoryCreateValidator, runValidation, createInventoryItem);
+router
+  .route(":/id")
+  .get(getItemById)
+  .delete(deleteInventoryItem)
+  .put(inventoryCreateValidator, runValidation, updateInventoryItem);
+router.route("/send-email").post(sendEmail);
+
+module.exports = router;
